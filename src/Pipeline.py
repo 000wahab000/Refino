52c9e7a1124158724d9efc960418d442993b69d4
000wahab000 <shaikhwhabshafi@gmail.com>
2026-07-16 13:06:45 +0530

    feat: implement Pipeline.py and clean stock data entries

--- src/Pipeline.py
+++ src/Pipeline.py
@@ -1,6 +1,10 @@
 # pyrefly: ignore [missing-import]
 import yfinance as yf
 import datetime as dt
+import pandas as pd
+pd.set_option('display.max_columns', None) 
+pd.set_option('display.width', 1000)
+pd.set_option('display.colheader_justify', 'center')
 
 start_date = "2025-01-01"
 end_date = dt.date.today()
@@ -12,13 +16,19 @@ companies = ["RELIANCE.NS", "IOC.NS", "BPCL.NS", "HINDPETRO.NS"]
 df_stocks = df.loc[:, (slice(None), companies)]
 df_stacks = df_stocks.stack(level=1)
 
-brent_close = df["Close"]["BZ=F"].ffill().bfill()
-usdinr_close = df["Close"]["USDINR=X"].ffill().bfill()
+brent_close = df["Close"]["BZ=F"].ffill()
+usdinr_close = df["Close"]["USDINR=X"].ffill()
 
 df_stacks["Brent_Crude"] = df_stacks.index.get_level_values("Date").map(brent_close)
 df_stacks["USD_INR"] = df_stacks.index.get_level_values("Date").map(usdinr_close)
 
 df_flat = df_stacks.reset_index()
 df_flat["Daily_Change"] = df_flat["Close"] - df_flat["Open"]
-df_flat.to_csv("cleaned_stock_data.csv",index=False)
-print(df_flat[["Date","Ticker","Open","Close","Daily_Change","Brent_Crude","USD_INR"]].tail(10))
+
+combined_df = df_flat.drop_duplicates(subset=["Date", "Ticker"], keep="last")
+
+combined_df.to_csv("raw_stock_data.csv", index=False)
+
+rounded_df = combined_df.round(3)
+rounded_df.to_csv("cleaned_stock_data.csv", index=False)
+print(f"Successfully updated! Total rows now in file: {len(combined_df)}")
